21ac180fea50fb953bbbe9667b5498cee67a9728
Alesha Reed <aleshak83@gmail.com>
2020-03-25 20:27:20 -0500

    Add project files.

---
+++ Syntactic Sugar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Syntactic_Sugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome All You Bugs");
+            var rabbitPrey = new List<string>()
+            {
+                "foxes",
+                "cats"
+            };
+
+            var rabbitPreditors = new List<string>()
+            {
+                "foxes",
+                "cats"
+            };
+
+            var bugs = new Bugs("Alesha", "mammels", rabbitPrey, rabbitPreditors);
+            Console.WriteLine(bugs.Eat("cake"));
+        }
+    }
+}
